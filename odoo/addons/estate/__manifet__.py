112a49d3f2314562bde097e9ade0b3d97163f71d
JPS6847 <146823181+JPS6847@users.noreply.github.com>
2026-02-17 12:10:47 +0100

    Create __manifet__.py

---
+++ odoo/addons/estate/__manifet__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Daniel",
+    'category': 'Category',
+    'description': """
+    Módulo estate para una inmobiliaria
+    """,
+    
+    # data files always loaded at installation
+    'data': [
+        # Añadir fichero
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        # Añadir fichero
+    ],
+    'aplication': True
+}
